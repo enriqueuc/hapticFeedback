56d1a1a0585df3257badcfa223fe1e9571bee840
Enrique Ugalde <enrique@ekt1136648mac.local>
2017-02-15 12:35:31 -0600

    - Haptic code

--- HapticFeedback/ViewController.swift
+++ HapticFeedback/ViewController.swift
@@ -8,18 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    var i = 0
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let btn = UIButton()
+        btn.frame = CGRect(x: (view.bounds.width-100.0)/2, y: (view.bounds.height-100.0)/2, width: 100.0, height: 100.0)
+        btn.setTitle("Try Haptic!", for: .normal)
+        btn.backgroundColor = UIColor.red
+        btn.setTitleColor(UIColor.white, for: .normal)
+        btn.addTarget(self, action: #selector(tapped), for: .touchUpInside)
+        
+        view.addSubview(btn)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tapped()
+    {
+        i += 1
+        //validate if the device supports the haptic
+        let device : Int = UIDevice.current.value(forKey: "_feedbackSupportLevel") as! Int
+        /*returns 2 for iPhone 7, 1 for iPhone 6 and 0 for earlier models*/
+        switch device
+        {
+        case 2:
+            print("feedback maybe available")
+            print("Running \(i)")
+                switch self.i
+                {
+                case 1:
+                    let generator = UINotificationFeedbackGenerator()
+                    generator.prepare() //If you don't do this, and there is a slight delay between the visual effect and the matching haptic, it might confuse users more than it helps.
+                    generator.notificationOccurred(.error)
+                    
+                case 2:
+                    let generator = UINotificationFeedbackGenerator()
+                    generator.prepare()
+                    generator.notificationOccurred(.success)
+                    
+                case 3:
+                    let generator = UINotificationFeedbackGenerator()
+                    generator.prepare()
+                    generator.notificationOccurred(.warning)
+                    
+                case 4:
+                    let generator = UIImpactFeedbackGenerator(style: .light)
+                    generator.prepare()
+                    generator.impactOccurred()
+                    
+                case 5:
+                    let generator = UIImpactFeedbackGenerator(style: .medium)
+                    generator.prepare()
+                    generator.impactOccurred()
+                    
+                case 6:
+                    let generator = UIImpactFeedbackGenerator(style: .heavy)
+                    generator.prepare()
+                    generator.impactOccurred()
+                    
+                default:
+                    let generator = UISelectionFeedbackGenerator()
+                    generator.selectionChanged()
+                    generator.prepare()
+                    self.i = 0
+                }
+            break
+        default:
+            print("feedback not available")
+            break
+        }
     }
-
-
 }
-
